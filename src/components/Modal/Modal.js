e64d7a9489280b99c2cee01c2207e1c067cf75dc
Ahmad Rifqy Syarwani <arifqys@gmail.com>
2020-04-22 09:21:36 +0700

    fix height popup & add close button

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -9,6 +9,7 @@ import styles from './Modal.module.css'
 const StyledPopup = styled(Popup)`
   &-content {
     overflow-y: auto;
+    max-height: 90% !important;
     border-radius: 10px;
   }
   @media (max-width: 500px) {
@@ -21,25 +22,30 @@ const StyledPopup = styled(Popup)`
 
 const Modal = (props) => (
     <StyledPopup trigger={props.trigger} modal>
-        <div className={styles.modal}>
-            <div>
-                <h2>{props.data.recipe.label}</h2>
-                <span className={styles.label} style={{backgroundColor: '#00c851'}}><AiOutlineClockCircle /> {props.data.recipe.totalTime} minutes</span>
-                <span className={styles.label} style={{backgroundColor: '#ff3547'}}><GiFlamer /> {Math.round(props.data.recipe.calories)} calories</span>
-                <img src={props.data.recipe.image} alt={props.data.recipe.label} />
+        {close => (
+            <div className={styles.modal}>
+                <span className={styles.close} onClick={close}>
+                &times;
+                </span>
+                <div>
+                    <h2>{props.data.recipe.label}</h2>
+                    <span className={styles.label} style={{backgroundColor: '#00c851'}}><AiOutlineClockCircle /> {props.data.recipe.totalTime} minutes</span>
+                    <span className={styles.label} style={{backgroundColor: '#ff3547'}}><GiFlamer /> {Math.round(props.data.recipe.calories)} calories</span>
+                    <img src={props.data.recipe.image} alt={props.data.recipe.label} />
+                </div>
+                <div>
+                    <h3>Ingredients</h3>
+                    <ul>
+                    {props.data.recipe.ingredientLines.map((item) => (
+                        <li key={Math.random()}>{item}</li>
+                    ))}
+                    </ul>
+                </div>
+                <div className={styles.button}>
+                    <a href={props.data.recipe.url} target="_blank" rel="noopener noreferrer">Full Recipe</a>
+                </div>
             </div>
-            <div>
-                <h3>Ingredients</h3>
-                <ul>
-                {props.data.recipe.ingredientLines.map((item) => (
-                    <li key={Math.random()}>{item}</li>
-                ))}
-                </ul>
-            </div>
-            <div className={styles.button}>
-                <a href={props.data.recipe.url} target="_blank" rel="noopener noreferrer">Full Recipe</a>
-            </div>
-        </div>
+        )}
     </StyledPopup>
 )
 
